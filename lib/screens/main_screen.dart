3f4644c9c713c63ac4d013ad18fe2b50a4921cf3
LuisEscobar05 <181099@ids.upchiapas.edu.mx>
2021-07-22 03:45:12 -0500

    add toast, and startConecticon method fixed

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/services.dart';
 import 'package:qr_wifi_share/constants.dart';
 import 'package:qr_wifi_share/screens/generate_qr_screen.dart';
 import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+import 'package:qr_wifi_share/my_toast.dart';
 import 'package:wifi_iot/wifi_iot.dart';
 
 class MainScreen extends StatelessWidget {
@@ -20,9 +21,8 @@ class MyHome extends StatefulWidget {
 }
 
 class MyHomeState extends State<MyHome> {
-  String _scanBarcode = "";
-  String result = "noconnected";
-  bool done = false;
+  String scanBarcode = "unknown";
+  CustomToast toast = new CustomToast();
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -107,7 +107,7 @@ class MyHomeState extends State<MyHome> {
                       ),
                     ),
                     Padding(padding: EdgeInsets.symmetric(vertical: 25)),
-                    Text(result),
+                    Text(scanBarcode),
                   ],
                 ),
               ],
@@ -123,8 +123,10 @@ class MyHomeState extends State<MyHome> {
     try {
       barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
           '#ff6666', 'Cancel', true, ScanMode.QR);
-      print(barcodeScanRes);
-      startConection();
+      setState(() {
+        this.scanBarcode = barcodeScanRes;
+      });
+      startConection(barcodeScanRes);
     } on PlatformException {
       barcodeScanRes = 'Failed to get platform version.';
     }
@@ -133,48 +135,15 @@ class MyHomeState extends State<MyHome> {
     // message was in flight, we want to discard the reply rather than calling
     // setState to update our non-existent appearance.
     if (!mounted) return;
-
-    setState(() {
-      this._scanBarcode = barcodeScanRes;
-    });
   }
 
-  void startConection() async {
-    var isConnected = await WiFiForIoTPlugin.connect("Home",
-        security: NetworkSecurity.WPA, password: "4815162342@@##");
+  void startConection(String data) async {
+    List<String> credentials = data.split(",");
+    var isConnected = await WiFiForIoTPlugin.connect(credentials[0],
+        security: NetworkSecurity.WPA, password: credentials[1]);
 
-    print("#########################:'$isConnected'");
     if (isConnected) {
-       _showMyDialog();
-      setState(() {
-        this.result="connect";
-      });
+      toast.toastInfo("Se ha conectado exitosamente a la red");
     }
   }
-  Future<void> _showMyDialog() async {
-  return showDialog<void>(
-    context: context,
-    barrierDismissible: false, // user must tap button!
-    builder: (BuildContext context) {
-      return AlertDialog(
-        title: const Text('QR Wifi Share Alert'),
-        content: SingleChildScrollView(
-          child: ListBody(
-            children: const <Widget>[
-              Text('Te has conectado a la red exitosamente'),
-            ],
-          ),
-        ),
-        actions: <Widget>[
-          TextButton(
-            child: const Text('Ok'),
-            onPressed: () {
-              Navigator.of(context).pop();
-            },
-          ),
-        ],
-      );
-    },
-  );
-}
 }
