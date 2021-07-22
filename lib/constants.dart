9972c2d0dc44491c21bd023690fdd4bdd4be4aea
LuisEscobar05 <181099@ids.upchiapas.edu.mx>
2021-07-22 03:42:36 -0500

    remove colors

--- lib/constants.dart
+++ lib/constants.dart
@@ -3,14 +3,5 @@ import 'package:flutter/material.dart';
 const firstColor = Color(0x00010100);
 const secondaryColor = Color(0xFFF9556A);
 const textColor = Color(0xDDD6F5FD);
-const warninngColor = Color(0xFFF3BB1C);
-const errorColor = Color(0xFFF03738);
-
-// const firstColor = Color(0x222f59ff);
-// const secondaryColor = Color(0xddd1e6fe);
-// const textColor = Color(0x00010001);
-// const warninngColor = Color(0xFFF3BB1C);
-// const errorColor = Color(0xFFF03738);
-
 const defaultWidthButton = 200.0;
 const defaultHeightButton = 50.0;
